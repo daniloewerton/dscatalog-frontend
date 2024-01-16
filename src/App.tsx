007cf1883aaf5ff8eee868f2678ebd3b85047097
Danilo Ewerton <danilo.ewe@gmail.com>
2024-01-16 11:07:51 -0300

    Implementando o navbar e menu hamburger

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './assets/styles/custom.scss';
-import Hello from 'components/Hello';
 import './App.css';
+import Navbar from 'components/Navbar';
+import Hello from 'components/Hello';
 
 function App() {
-
   return (
-    <Hello />
+    <>
+      <Navbar />
+      <Hello />
+    </>
   );
 }
 
