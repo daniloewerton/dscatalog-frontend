f3316e47b9b74d18c9a4529fa8d8622936e6c573
Danilo Ewerton <danilo.ewe@gmail.com>
2024-01-18 10:46:06 -0300

    estilização do botão

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,10 +1,16 @@
 import './styles.css';
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 
 const ButtunIcon = () => {
   return (
-    <button className="btn btn-primary btn-icon">
-      <h6>INICIE AGORA A SUA BUSCA</h6>
-    </button>
+    <div className="btn-container">
+      <button className="btn btn-primary">
+        <h6>inicie agora a sua busca</h6>
+      </button>
+      <div className="btn-icon-container">
+        <ArrowIcon />
+      </div>
+    </div>
   );
 };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,9 +15,11 @@ const Home = () => {
               mercado.
             </p>
           </div>
-          <Link to="/products">
-            <ButtunIcon />
-          </Link>
+          <div>
+            <Link to="/products">
+              <ButtunIcon />
+            </Link>
+          </div>
         </div>
         <div className="home-image-container">
           <MainImage />
