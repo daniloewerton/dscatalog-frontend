3ffbf266302f0b14d26436bf4b8e9ea0c18dfdd0
Danilo Ewerton <danilo.ewe@gmail.com>
2024-02-13 16:46:20 -0300

    redenrizando produtos do backend na página de catálogo

--- src/pages/ProductDetails/index.tsx
+++ src/pages/ProductDetails/index.tsx
@@ -8,18 +8,16 @@ import { BASE_URL } from 'util/requests';
 import { useEffect, useState } from 'react';
 
 const ProductDetails = () => {
-
   type UrlParams = {
     productId: string;
-  }
+  };
 
   const { productId } = useParams<UrlParams>();
 
   const [product, setProduct] = useState<Product>();
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/products/${productId}`)
-    .then(response => {
+    axios.get(`${BASE_URL}/products/${productId}`).then((response) => {
       setProduct(response.data);
     });
   }, [productId]);
@@ -36,22 +34,17 @@ const ProductDetails = () => {
         <div className="row">
           <div className="col-xl-6">
             <div className="img-container">
-              <img
-                src={product?.imgUrl}
-                alt={product?.name}
-              ></img>
+              <img src={product?.imgUrl} alt={product?.name}></img>
             </div>
             <div className="name-price-container">
               <h1>{product?.name}</h1>
-              { product && <ProductPrice price={product?.price} />}
+              {product && <ProductPrice price={product?.price} />}
             </div>
           </div>
           <div className="col-xl-6">
             <div className="description-container">
               <h2>Descrição do produto</h2>
-              <p>
-                {product?.description}
-              </p>
+              <p>{product?.description}</p>
             </div>
           </div>
         </div>
